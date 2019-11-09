42ebb03e6d34c6b22815af85c7a3dd10066fa501
Nahuel Verdugo Revigliono <nahuelvr@gmail.com>
2019-11-09 03:19:29 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# nahuelvr.github.com
-Personal website
---
+++ precache-manifest.1ff7cfdfa0c3fce25ac31d913583a403.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451476a6970e6f6dc44d6de528f06ad7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6dbc19ffe45c1f820a8",
+    "url": "/static/css/main.ce5eb764.chunk.css"
+  },
+  {
+    "revision": "5fa5c94206a8b766ddeb",
+    "url": "/static/js/2.5f2acc8d.chunk.js"
+  },
+  {
+    "revision": "c6dbc19ffe45c1f820a8",
+    "url": "/static/js/main.a1cb526b.chunk.js"
+  },
+  {
+    "revision": "f8c4fbdecd73b27ac118",
+    "url": "/static/js/runtime-main.52f5e485.js"
+  }
+]);
