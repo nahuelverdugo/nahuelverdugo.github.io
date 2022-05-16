7ff1d9d193c6f6aa8734ded0fc8a66db0e3684fc
Nahuel Verdugo <nahuelvr@gmail.com>
2022-05-16 22:04:51 +0200

    Updates

---
+++ precache-manifest.59ad4d129232737d88ee76e7b792ba85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8346e1fabca436daa96a744c5fe8af81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afc6f681770d3923c4e7",
+    "url": "/static/css/main.e6a3a05c.chunk.css"
+  },
+  {
+    "revision": "150184ed777679d71dc5",
+    "url": "/static/js/2.df49dbf0.chunk.js"
+  },
+  {
+    "revision": "afc6f681770d3923c4e7",
+    "url": "/static/js/main.9d379ebc.chunk.js"
+  },
+  {
+    "revision": "ca12f755a6f12f041e95",
+    "url": "/static/js/runtime-main.98a937aa.js"
+  }
+]);
